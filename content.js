6070c0eb8c2ad802f1891e328c4649cb45ab70d1
Jason Hansen <jbhansen016@gmail.com>
2022-03-05 21:12:33 -0700

    fixed up expandSelectionToWhitespace() helper

--- content.js
+++ content.js
@@ -1,24 +1,19 @@
 // FRONTEND
 window.addEventListener("mouseup", function (event) {
     var text = "";
-    if (window.getSelection) {
+    if (isTextSelectionRange()) {
         text = window.getSelection().toString();
-        var startIndex = document.body.innerText.indexOf(text);
-        var endIndex = startIndex + text.length;
-        while (new RegExp(/[a-zA-Z0-9]/, 'g').test(document.body.innerText.charAt(startIndex))) {
-            startIndex--;
+        if (!new RegExp(/[a-zA-Z0-9]/, 'g').test(text)) {
+            return;
         }
-        while (new RegExp(/[a-zA-Z0-9]/, 'g').test(document.body.innerText.charAt(endIndex))) {
-            endIndex++;
-        }
-        text = document.body.innerText.substring(startIndex, endIndex); // TODO this might have an off by one error
+        text = expandSelectionToWhitespace(text, document.body.innerText);
     }
-    else if (document.selection && document.selection.type != "Control") { // idk what this does...
-        text = document.selection.createRange().text;
-    }
+    // idk what this does...
+    // else if (window.getSelection() && window.getSelection().type != "Control") {
+    //     text = window.getSelection().addRange(text.length);
+    // }
 
     // sending message to background.js
-    text = text.trim();
     if (text.length > 0) {
         var message = {
             method: "persist-highlight",
@@ -42,3 +37,21 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+
+function expandSelectionToWhitespace(text, html) {
+    var startIndex = document.body.innerText.indexOf(text);
+    var endIndex = startIndex + text.length;
+    const isSpaceRegex = new RegExp(/\s/, 'g');
+    while (!isSpaceRegex.test(document.body.innerText.charAt(startIndex)) && startIndex > -1) {
+        startIndex--;
+    }
+    while (!isSpaceRegex.test(document.body.innerText.charAt(endIndex)) && endIndex < document.body.innerText.length) {
+        endIndex++;
+    }
+    text = document.body.innerText.substring(startIndex, endIndex);
+    return text.trim();
+}
+
+function isTextSelectionRange() {
+    return window.getSelection() && window.getSelection().type == 'Range';
+}
